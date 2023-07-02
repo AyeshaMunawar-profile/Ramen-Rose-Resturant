812247e3aa454b5a776cad9c53ad94f698807073
Ayesha Munawar <54625611+AyeshaMunawar-profile@users.noreply.github.com>
2023-07-02 18:03:35 +0500

    RR> Add sample
    RR> Add sample

--- src/app/components/recipe-book/recipe-list/recipe-list.component.ts
+++ src/app/components/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
-
+import data from "../../../../assets/recipeData.json"
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  public recipesList: any = null;
+
 
   constructor() { }
-
-  ngOnInit(): void {
+   ngOnInit(): void {
+    this.recipesList= data;
   }
-
 }
--- src/app/components/recipe-book/recipe-list/recipe/recipe.component.ts
+++ src/app/components/recipe-book/recipe-list/recipe/recipe.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-recipe',
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe.component.scss']
 })
 export class RecipeComponent implements OnInit {
-
+  @Input() recipeDetails:any;
+  public fileImageDefaultPath:string= "../../../../../assets/food_images/"
   constructor() { }
 
   ngOnInit(): void {
+    this.fileImageDefaultPath= this.fileImageDefaultPath + this.recipeDetails.field5 + ".jpg"
   }
 
 }
